4844894d22adc6988461ef6d1e14d3b992df20ee
Vanthu204 <lythu2k4lc@gmail.com>
2025-03-05 15:45:09 +0700

    commit test

---
+++ src/Screens/Demogit.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Demogit = () => {
+  return (
+    <View>
+      <Text>Demogit</Text>
+    </View>
+  )
+}
+
+export default Demogit
+
+const styles = StyleSheet.create({})
